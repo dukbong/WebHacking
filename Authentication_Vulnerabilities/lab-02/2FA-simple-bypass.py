3e5afb96e73ee570714308345c0f34f54dd5b5e2
Hyeon Sung <gorae930301@gmail.com>
2025-07-18 16:03:18 +0900

    Update 2FA-simple-bypass.py

--- Authentication_Vulnerabilities/lab-02/2FA-simple-bypass.py
+++ Authentication_Vulnerabilities/lab-02/2FA-simple-bypass.py
@@ -18,12 +18,12 @@ def print_lab_info(url):
   You have already obtained a valid username and password,
   but do not have access to the user's 2FA verification code.
   To solve the lab, access Carlos's account page.
-
- Your credentials:
-  - wiener : peter
-
- Victim's credentials:
-  - carlos : montoya
+ 
+  Your credentials:
+   - wiener : peter
+ 
+  Victim's credentials:
+   - carlos : montoya
 
 +{'='*70}+
 """
