c959236db3ca004f548c2598bb93aada28763171
Hyeon Sung <gorae930301@gmail.com>
2025-07-18 16:02:46 +0900

    Update Username-enumeration-via-different-responses.py

--- Authentication_Vulnerabilities/lab-01/Username-enumeration-via-different-responses.py
+++ Authentication_Vulnerabilities/lab-01/Username-enumeration-via-different-responses.py
@@ -12,14 +12,15 @@ def print_lab_info(url):
  PortSwigger Lab: Authentication vulnerabilities
  Challenge: Username enumeration via different responses
  Target: {url}
+ 
+ Lab Description:
+  This lab is vulnerable to username enumeration and password brute-force attacks. 
+  It has an account with a predictable username and password, which can be found in the following wordlists:
 
- This lab is vulnerable to username enumeration and password brute-force attacks. 
- It has an account with a predictable username and password, which can be found in the following wordlists:
+  - Candidate usernames
+  - Candidate passwords
 
- - Candidate usernames
- - Candidate passwords
-
- To solve the lab, enumerate a valid username, brute-force this user's password, then access their account page.
+  To solve the lab, enumerate a valid username, brute-force this user's password, then access their account page.
 
 +{'='*70}+
 """
